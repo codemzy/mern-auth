a489e1409ba7c727bd7b71b2db0b0f5b04ff770c
Emma J <codemzy@gmail.com>
2016-10-18 15:49:59 +0000

    Updated front end login router routes

--- app/login/config/router.js
+++ app/login/config/router.js
@@ -12,12 +12,12 @@ var Account = require('../components/Account');
 // Routes
 var routes = (
     <Router history={browserHistory}> 
-        <Route path="/app" component={Main}>
+        <Route path="/" component={Main}>
             <IndexRoute component={LogIn} />
-            <Route path='/app/login' header='Log In' component={LogIn} />
-            <Route path='/app/login/forgotten' header='Forgotten Password' component={ForgotPW} />
-            <Route path='/app/register' header='Register' component={Register} />
-            <Route path='/app/account' header='Account' component={Account} />
+            <Route path='/login' header='Log In' component={LogIn} />
+            <Route path='/login/forgotten' header='Forgotten Password' component={ForgotPW} />
+            <Route path='/login/register' header='Register' component={Register} />
+            <Route path='/login/account' header='Account' component={Account} />
         </Route>
     </Router>
 );
