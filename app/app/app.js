41e2946704cee78a2326c305d38d1bb63e34bc25
Emma J <codemzy@gmail.com>
2016-10-19 16:12:12 +0000

    Configured store to app

--- app/app/app.js
+++ app/app/app.js
@@ -1,7 +1,16 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const {Provider} = require('react-redux');
+
+// Load redux store
+import { configure } from './store/config_store';
+const store = configure();
 
 // routes
 import routes from './config/router';
 
-ReactDOM.render(routes, document.getElementById('app'));
+// if (!store.getState().user.email) {
+//     // dispatch action to fetch user data
+// }
+
+ReactDOM.render(<Provider store={store}>{routes}</Provider>, document.getElementById('app'));
