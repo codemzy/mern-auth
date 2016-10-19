dc55c9376362f3b6c7c833c0f32e039a781850a4
Emma J <codemzy@gmail.com>
2016-10-19 16:22:21 +0000

    Wired up fetchUserInfo action to app

--- app/app/app.js
+++ app/app/app.js
@@ -5,12 +5,16 @@ const {Provider} = require('react-redux');
 // Load redux store
 import { configure } from './store/config_store';
 const store = configure();
+// Load actions
+import { fetchUserInfo } from './actions/actions_user';
 
 // routes
 import routes from './config/router';
 
-// if (!store.getState().user.email) {
-//     // dispatch action to fetch user data
-// }
+// if we dont have the user data, fetch it to state
+if (!store.getState().user.email) {
+    // dispatch action to fetch user data
+    store.dispatch(fetchUserInfo());
+}
 
 ReactDOM.render(<Provider store={store}>{routes}</Provider>, document.getElementById('app'));
