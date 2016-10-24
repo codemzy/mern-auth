306d17a441e291c4c6ce61a0f2834daeea7cb6e6
Emma J <codemzy@gmail.com>
2016-10-24 15:29:05 +0000

    Wired up remove alert

--- app/app/components/Alert.js
+++ app/app/components/Alert.js
@@ -1,7 +1,15 @@
 const React = require('react');
 import { connect } from 'react-redux';
 
+// Load actions
+import { removeAlert } from '../actions/actions_alert';
+
 class Alert extends React.Component {
+    
+    _removeAlert() {
+        this.props.dispatch(removeAlert());
+    }
+    
     render() {
         if (this.props.alerts.type) {
             // set the header for the alert
@@ -16,7 +24,7 @@ class Alert extends React.Component {
             }
             return (
                 <div className={"alert alert-dismissible alert-" + alertClass }>
-                  <button type="button" className="close" data-dismiss="alert">&times;</button>
+                  <button type="button" className="close" data-dismiss="alert" onClick={this._removeAlert.bind(this)}>&times;</button>
                   <strong>{alertHeader}</strong> {this.props.alerts.message}
                 </div>
             );
