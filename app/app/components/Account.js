59c52473af244ea1d8ebe6e0eb83bfe689b0fa5d
Emma J <codemzy@gmail.com>
2016-10-24 11:01:39 +0000

    Confirmed email message

--- app/app/components/Account.js
+++ app/app/components/Account.js
@@ -3,9 +3,18 @@ import { connect } from 'react-redux';
 
 class Account extends React.Component {
     render() {
+        let confirmEmail = () => {
+            if (!this.props.user.emailConfirmed) {
+                return <p className="text-warning"><i className="fa fa-exclamation-triangle" aria-hidden="true"></i> Confirm your email address.</p>;
+            } else {
+                return <p className="text-success"><i className="fa fa-check" aria-hidden="true"></i> Email confirmed.</p>;
+            }
+        };
         return (
             <div className="container text-center">
+                <p>You are logged in as...</p>
                 <h1>{this.props.user.email}</h1>
+                {confirmEmail()}
             </div>
         );
     }
