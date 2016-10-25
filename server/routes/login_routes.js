3d55989c85c41437c5ba6ddaf0e91519b33507b1
Emma J <codemzy@gmail.com>
2016-10-25 14:54:22 +0000

    Updated login_routes to redirect if user is logged in

--- server/routes/login_routes.js
+++ server/routes/login_routes.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+// services
+const passport = require('passport');
+require('../services/passport.js'); // this needs to be run but is not directly referenced in this file
+
+// session false as we are not using cookies, using tokens
+// redirect to app if user is auth'd
+const checkForUser = function(req, res) {
+    passport.authenticate('jwt', { session: false }, function (err, user, info) {
+        if (err) { 
+            return res.sendFile(process.cwd() + '/public/html/login.html');
+        }
+        if (user) {
+            return res.redirect('/app');
+        } else {
+            return res.sendFile(process.cwd() + '/public/html/login.html');
+        }
+    })(req, res);
+};
+
 // ROUTES -----------------------------------------------------
 
 router.get('/', function(req, res) {
-    res.sendFile(process.cwd() + '/public/html/login.html');
+    checkForUser(req, res);
 });
 
-router.get('/*', function(req, res) {
-    res.sendFile(process.cwd() + '/public/html/login.html');
+router.get('/*', function(req, res, next) {
+    checkForUser(req, res);
 });
 
 module.exports = router;
