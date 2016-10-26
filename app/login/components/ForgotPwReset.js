7b4443f3116ab7d146bea8826139c4b1d87e2b46
Emma J <codemzy@gmail.com>
2016-10-26 11:14:22 +0000

    Added front end validation to Reset Password component

--- app/login/components/ForgotPwReset.js
+++ app/login/components/ForgotPwReset.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 // API
 import {  } from '../api/user';
@@ -7,7 +8,8 @@ class ResetPw extends React.Component {
     constructor() {
         super();
         this.state = {
-            errors: {}
+            errors: {},
+            tokenValid: true
         };
     }
     
@@ -17,14 +19,31 @@ class ResetPw extends React.Component {
         return re.test(email);
     }
     
-    // TO DO CHECK IF TOKEN EXPIRED
+    _checkResetTime(link) {
+        // basic check to see if reset link still valid
+        const RESET_ARR = link.split("-");
+        if (RESET_ARR.constructor !== Array || RESET_ARR.length !== 3) {
+            return false;
+        }
+        const timestamp = RESET_ARR[0];
+        const now = new Date().getTime();
+        const difference = timestamp - now;
+        const timeLeft = Math.floor(difference / 1000 / 60) + 60;
+        return timeLeft < 1 ? false : true ;
+    }
     
     _handleFormSubmit(e) {
         e.preventDefault();
+        if (!this._checkResetTime(this.props.routeParams.token)) {
+            this.setState({
+                tokenValid: false
+            });
+            return false;
+        }
         let email = this.refs.email.value;
         let password = this.refs.password.value;
         let confirm = this.refs.confirm.value;
-        // check for errors
+        // check for errors in form
         const ERRORS = {};
         if (!email || !this._emailValid(email)) {
             ERRORS.email = "You need to enter a valid email address";
@@ -53,12 +72,14 @@ class ResetPw extends React.Component {
     }
     
     render() {
+        let alertMessage = this.state.tokenValid ? false : <div className="alert alert-dismissible alert-danger">Your reset link has expired, please request a new reset link via the <Link to="/app/login/forgotten" className="alert-link">forgotten password form</Link>.</div> ;
         let emailError = this.state.errors.email ? <div className="has-error"><p className="help-block">{this.state.errors.email}</p></div> : false ;
         let passwordError = this.state.errors.password ? <div className="has-error"><p className="help-block">{this.state.errors.password}</p></div> : false ;
         let confirmError = this.state.errors.confirm ? <div className="has-error"><p className="help-block">{this.state.errors.confirm}</p></div> : false ;
         return (
             <div className="container">
                 <div className="col-md-6 col-md-offset-3">
+                    { alertMessage }
                     <h1 className="page-title">Reset Your Password</h1>
                     <form onSubmit={this._handleFormSubmit.bind(this)}>
                         <div className="form-group">
--- app/login/config/router.js
+++ app/login/config/router.js
@@ -5,7 +5,8 @@ var {Route, Router, IndexRoute, browserHistory} = require('react-router');
 // Components
 var Main = require('../components/Main');
 var LogIn = require('../components/LogIn');
-var ForgotPW = require('../components/ForgotPw');
+var ForgotPw = require('../components/ForgotPw');
+var ForgotPwReset = require('../components/ForgotPwReset');
 var Register = require('../components/Register');
 var Account = require('../components/Account');
 
@@ -15,7 +16,8 @@ var routes = (
         <Route path="/" component={Main}>
             <IndexRoute component={LogIn} />
             <Route path='/login' header='Log In' component={LogIn} />
-            <Route path='/login/forgotten' header='Forgotten Password' component={ForgotPW} />
+            <Route path='/login/forgotten' header='Forgotten Password' component={ForgotPw} />
+            <Route path='/login/forgotten/reset/:token' header='Reset Password' component={ForgotPwReset} />
             <Route path='/login/register' header='Register' component={Register} />
             <Route path='/login/account' header='Account' component={Account} />
         </Route>
