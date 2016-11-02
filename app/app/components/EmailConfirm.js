ba9495d4e145dffe82f143bf7a39292c62ca829e
Emma J <codemzy@gmail.com>
2016-11-02 16:50:05 +0000

    Connected email confirm to state to check if user email confirmed before sending request to api

--- app/app/components/EmailConfirm.js
+++ app/app/components/EmailConfirm.js
@@ -7,16 +7,22 @@ import { addAlert, removeAlert } from '../actions/actions_alert';
 
 class EmailConfirm extends React.Component {
     
-    componentDidMount() {
-        const ECC = this.props.routeParams.emailCode;
-        this.props.dispatch(confirmEmailCode(ECC))
-            .then((success) => {
-                this.props.dispatch(removeAlert());
-                this.context.router.push('/app/account');
-            }, (error) => {
-                this.props.dispatch(addAlert("Email confirmation code invalid, try again.", "error"));
-                this.context.router.push('/app/account');
-            });
+    componentDidUpdate() {
+        if (this.props.user.email && this.props.user.emailConfirmed) {
+            // if the email is already confirmed
+            this.props.dispatch(addAlert("Your email is confirmed. Thank you.", "success"));
+            this.context.router.push('/app/account');
+        } else if (this.props.user.email) {
+            const ECC = this.props.routeParams.emailCode;
+            this.props.dispatch(confirmEmailCode(ECC))
+                .then((success) => {
+                    this.props.dispatch(removeAlert());
+                    this.context.router.push('/app/account');
+                }, (error) => {
+                    this.props.dispatch(addAlert("Email confirmation code invalid, try again.", "error"));
+                    this.context.router.push('/app/account');
+                });
+        }
     }
     
     render() {
@@ -33,4 +39,8 @@ EmailConfirm.contextTypes = {
     router: React.PropTypes.object.isRequired
 };
 
-export default connect()(EmailConfirm);
+export default connect((state) => {
+    return {
+        user: state.user
+    };
+})(EmailConfirm);
