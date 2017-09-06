8bc55028d65680ba386b88fc8230addcc7a2fe96
Emma J <codemzy@gmail.com>
2017-09-06 08:42:25 +0000

    Updated app routes to return login if not auth

--- server/routes/app_routes.js
+++ server/routes/app_routes.js
@@ -6,18 +6,39 @@ const passport = require('passport');
 require('../services/passport.js'); // this needs to be run but is not directly referenced in this file
 
 // session false as we are not using cookies, using tokens
-// check if user is auth'd, if not redirect
-const checkAuth = passport.authenticate('jwt', { session: false, failureRedirect: '/login' });
-
+// redirect to app if user is auth'd
+const checkUserAuth = function(req, res) {
+    passport.authenticate('jwt', { session: false }, function (err, user, info) {
+        if (err) { 
+            // problem with auth so try login
+            // clear the user object on the req and the cookie (if expired one held)
+            req.logout();
+            res.clearCookie('jwt', { httpOnly: true, secure: true });
+            return res.sendFile(process.cwd() + '/public/html/login.html');
+        }
+        if (user) {
+            // they are auth'd and can access the app
+            return res.sendFile(process.cwd() + '/public/html/app.html');
+        } else {
+            // no user so need to login
+            // clear the user object on the req and the cookie (if expired one held)
+            req.logout();
+            res.clearCookie('jwt', { httpOnly: true, secure: true });
+            return res.sendFile(process.cwd() + '/public/html/login.html');
+        }
+    })(req, res);
+};
 
 // ROUTES -----------------------------------------------------
 
-router.get('/', checkAuth, function(req, res) {
-    res.sendFile(process.cwd() + '/public/html/app.html');
+router.get('/', function(req, res) {
+    // check if the user has an active role
+    checkUserAuth(req, res);
 });
 
-router.get('/*', checkAuth, function(req, res) {
-    res.sendFile(process.cwd() + '/public/html/app.html');
+router.get('/*', function(req, res) {
+    // check if the user has an active role
+    checkUserAuth(req, res);
 });
 
 module.exports = router;
