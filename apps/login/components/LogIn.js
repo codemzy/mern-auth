83b992efdbcbdc512c79a351e422b10a1750eccb
Emma J <codemzy@gmail.com>
2018-10-02 15:13:51 +0000

    Fixed forgotton pw link from login page

--- apps/login/components/LogIn.js
+++ apps/login/components/LogIn.js
@@ -89,7 +89,7 @@ class LogIn extends React.Component {
                         <input className="btn btn-primary btn-block" type="submit" value="Log In" />
                     </div>
                 </form>
-                <p><small><Link to="/login/forgotten" activeClassName="active">Forgot your password?</Link></small></p>
+                <p><small><Link to="/app/login/forgotten" activeClassName="active">Forgot your password?</Link></small></p>
             </Wrap>
         );
     }
